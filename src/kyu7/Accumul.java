568ac1ffbfa190cafcc39adc695db7795813b2c8
Sergey Tsekhmistrenko <ts.seroops@gmail.com>
2021-05-31 22:22:45 +0300

    version3 31.05

--- src/kyu7/Accumul.java
+++ src/kyu7/Accumul.java
@@ -1,2 +1,21 @@
-package kyu7;public class Accumul {
+package kyu7;
+
+//www.codewars.com/kata/5667e8f4e3f572a8f2000039
+
+public class Accumul {
+    public static void main(String[] args) {
+        System.out.println(accum("dfErf" ));
+    }
+
+    public static String accum(String s) {
+        String accum = "";
+        for (int i = 0; i < s.length() ; i++) {
+            accum += s.toUpperCase().toCharArray()[i];
+            for (int j = 0; j < i; j++) {
+                accum += s.toLowerCase().toCharArray()[i];
+            }
+            if(i+1 != s.length())accum +="-";
+        }
+        return accum;
+    }
 }
--- src/kyu7/Troll.java
+++ src/kyu7/Troll.java
@@ -1,2 +1,13 @@
-package kyu7;public class Troll {
+package kyu7;
+
+//www.codewars.com/kata/52fba66badcd10859f00097e
+
+public class Troll {
+    public static void main(String[] args) {
+        System.out.println(disemvowel("This website is for losers LOL!"));
+    }
+
+    public static String disemvowel(String str) {
+        return str.replaceAll("[AaEeOoUuIi]", "");
+    }
 }
