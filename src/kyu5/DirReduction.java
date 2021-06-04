9e6ae3fbc4cce261d85edb90e7c2aa4522b63d90
Sergey Tsekhmistrenko <ts.seropps@gmail.com>
2021-06-04 23:08:24 +0300

    version1 04.06

--- src/kyu5/DirReduction.java
+++ src/kyu5/DirReduction.java
@@ -2,43 +2,24 @@ package kyu5;
 
 //https://www.codewars.com/kata/550f22f4d758534c1100025a
 
+
 import java.util.*;
 
 public class DirReduction {
     public static void main(String[] args) {
-        System.out.println(Arrays.toString(dirReduc(new String[]{"NORTH", "EAST", "SOUTH", "WEST", "SOUTH", "SOUTH", "NORTH", "WEST"})));
+        System.out.println(Arrays.toString(dirReduc(new String[]
+                {"NORTH", "SOUTH", "SOUTH", "EAST", "WEST", "NORTH", "WEST"})));
     }
     public static String[] dirReduc(String[] arr) {
-        List<String> list = new LinkedList<>(Arrays.asList(arr));
+       List<String> list = new ArrayList<>(Arrays.asList(arr));
 
-        int n =  Collections.frequency(list, "NORTH");
-        int s =  Collections.frequency(list, "SOUTH");
-        int w =  Collections.frequency(list, "WEST");
-        int e =  Collections.frequency(list, "EAST");
-        if (n >= s) {
-            for (int i = 0; i < s; i++) {
-                list.remove("NORTH");
-                list.remove("SOUTH");
-            }
-        }
-        else {
-            for (int i = 0; i < n; i++) {
-                list.remove("SOUTH");
-                list.remove("NORTH");
-            }
-        }
-        if (w >= e) {
-            for (int i = 0; i < e; i++) {
-                list.remove("WEST");
-                list.remove("EAST");
-            }
-        }
-        else {
-            for (int i = 0; i < w; i++) {
-                list.remove("EAST");
-                list.remove("WEST");
-            }
-        }
+       for (int i = 0; i < list.size() - 1; i++) {
+           if ("NORTH:SOUTH,EAST:WEST,SOUTH:NORTH,WEST:EAST".contains(list.get(i) + ":" + list.get(i + 1))) {
+               list.remove(i+1);
+               list.remove(i);
+               i=-1;
+           }
+       }
         return list.toArray(new String[list.size()]);
     }
 }
